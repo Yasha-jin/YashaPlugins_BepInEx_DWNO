68c79483a9d455d332d37d4357f9c9e97305ad21
Yashajin <68703144+Yasha-jin@users.noreply.github.com>
2023-03-23 22:05:04 +0100

    Remove config file, unnecessary tbh

--- EnableVSync/Plugin.cs
+++ EnableVSync/Plugin.cs
@@ -13,22 +13,11 @@ public class Plugin : BasePlugin
     internal const string PluginName = "EnableVSync";
     internal const string PluginVersion = "1.0";
 
-    private static ConfigEntry<VSyncList> ConfigVSync { get; set; }
-
-    private enum VSyncList
-    {
-        On = 1,
-        Off = 0
-    }
-
     public override void Load()
     {
-        // ConfigBinding
-        ConfigVSync = Config.Bind("General", "VSync", VSyncList.On);
-
         // Plugin startup logic
         Log.LogInfo($"Plugin {GUID} is loaded!");
 
-        QualitySettings.vSyncCount = (int)ConfigVSync.Value;
+        QualitySettings.vSyncCount = 1;
     }
 }
