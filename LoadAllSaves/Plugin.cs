1d75c0e96c1cfaf732f635e790becc206875d0d7
Yashajin <68703144+Yasha-jin@users.noreply.github.com>
2023-11-28 12:17:24 +0100

    Add LoadAllSaves

---
+++ LoadAllSaves/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using static PlayerData;
+using static StorageData;
+
+namespace LoadAllSaves;
+
+[BepInPlugin(GUID, PluginName, PluginVersion)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    internal const string GUID = "LoadAllSaves";
+    internal const string PluginName = "LoadAllSaves";
+    internal const string PluginVersion = "1.0.0";
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {GUID} is loaded!");
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+    }
+
+    [HarmonyPatch(typeof(CSaveDataHeader), "ReadSaveData")]
+    [HarmonyPostfix]
+    public static void ReadSaveData_Postfix(CSaveDataHeader __instance)
+    {
+        __instance.m_isMatchStaemId = true;
+    }
+}
